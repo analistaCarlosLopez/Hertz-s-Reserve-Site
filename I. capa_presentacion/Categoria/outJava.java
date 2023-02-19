d8de5fd7f261af810fb054a5ae49a1ade928ccf0
analistaCarlosLopez <carlos.palada90@unitec.edu>
2023-02-18 18:52:59 -0600

    EXAMEN II

---
+++ I. capa_presentacion/Categoria/outJava.java
@@ -0,0 +1,38 @@
+package hello;
+import java.util.Scanner;
+public class Hello{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int j;
+        boolean jose4;
+        boolean jose5;
+        boolean jose7;
+        boolean jose8;
+        int entero1;
+        int entero2;
+        int enteroguanil;
+        System.out.println("Hello World");
+        i = 'h';
+        jose4 = 4+4*(4+2)/2;
+        jose7 = true;
+        enteroguanil = true;
+        i = j;
+        jose4 = jose5;
+        enteroguanil = jose7;
+        i = 6;
+        i = sc.nextInt();
+        for(i = 0; i < 10; i++) {
+            System.out.println("hola jose antonio");
+        }
+        while((i<10)){
+            System.out.println("entrando");
+            for(i = 0; i < 10; i++) {
+                System.out.println("hola jose antonio");
+                if((jose5<>'h')){
+                    System.out.println("adios jose antonio");
+                }
+            }
+        }
+    }
+}
